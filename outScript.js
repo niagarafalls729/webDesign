124ed0f381a63d469510a610ef65c6fcf6bc7f69
niagarafalls729 <wltn729@gmail.com>
2023-11-12 08:59:25 +0900

    대만여행중 2 # 자다가 못한거 화나서 일어나서 결국함 .

--- outScript.js
+++ outScript.js
@@ -1,16 +1,15 @@
 $(function(){
-    let currentIndex = 0;   //현재 이미지 설정
-    $(".sliderWrap").append($(".slider").first().clone(true));  //첫번째 이미지를 복사해서 마지막에 추가
+   let index = 0 ;
+   $('.sliderWrap').append($('.slider').first().clone(true));
 
-    setInterval(function(){     //3초에 한번씩 실행   
-        currentIndex++;     //현재 이미지를 1씩 증가
-        $(".sliderWrap").animate({marginTop: -currentIndex * 300 + "px"}, 600);     //이미지 애니메이션
-
-        if(currentIndex == 3){  //마지막 이미지
-            setTimeout(function(){
-                $(".sliderWrap").animate({ marginTop:0 }, 0);   //애니메이션 초기화
-                currentIndex = 0;   //현재 이미지를 초기화
-            }, 700)
+   setInterval(function(){
+        index++;
+        console.log("dddd")
+        $('.sliderWrap').animate({marginTop:'-'+(index*300 )+'px'},400)
+        if(index ==3 ){
+            $('.sliderWrap').animate({marginTop:'0px'},0)
+            index=0;
         }
-    }, 3000);
+
+   },3000)
 })
