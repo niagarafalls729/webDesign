7943c9ba99e4cccae6f7382fec1b0c40e8779d94
niagarafalls729 <wltn729@gmail.com>
2023-11-13 23:59:25 +0900

    대만여행중 3# 슬라이드 나혼자 그냥해봄 , 내일 다시 한국 간다. 뱅기에서 공부좀해야할듯.

--- outScript.js
+++ outScript.js
@@ -1,15 +1,31 @@
+// 이건 s-1유형
+// $(function(){
+//     let index = 0;
+
+//     setInterval(function(){
+
+//         $('.slider').eq(index).fadeOut()
+//         $('.slider').eq(index+1).fadeIn()
+//         index++;
+
+//         if(index==2){
+//             index=0
+//         }
+//     },3000)
+// })
+
 $(function(){
-   let index = 0 ;
-   $('.sliderWrap').append($('.slider').first().clone(true));
+    let index=1;
 
-   setInterval(function(){
+    $('.sliderWrap').append($('.slider').eq(0).clone(true))
+
+    setInterval(function(){
+        
+        $('.sliderWrap').animate({marginLeft:'-'+index*1200+ 'px'},300)
         index++;
-        console.log("dddd")
-        $('.sliderWrap').animate({marginTop:'-'+(index*300 )+'px'},400)
-        if(index ==3 ){
-            $('.sliderWrap').animate({marginTop:'0px'},0)
-            index=0;
+        if(index==4){
+            index=1;
+            $('.sliderWrap').animate({marginLeft:'0px'},0)
         }
-
-   },3000)
+    },3000)
 })
