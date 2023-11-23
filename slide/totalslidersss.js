2730043ef604c7887d5ac72f4df48550e347c174
niagarafalls729 <wltn729@gmail.com>
2023-11-23 11:09:43 +0900

    슬라이드 연습1

--- slide/totalslidersss.js
+++ slide/totalslidersss.js
@@ -15,5 +15,30 @@ $(function(){
 
 
 
+    console.log("z",$('.sliderWrap2 > .slider').eq(0))
     
+
+    let index_two = 1;
+    $('.sliderWrap2').append($('.sliderWrap2 > .s1').clone(true));
+ 
+    setInterval(function(){
+        $('.sliderWrap2 ').animate({marginLeft: index_two*-100+'%'})
+        index_two++;
+        if(index_two==4){
+            index_two=1;
+            $('.sliderWrap2').animate({marginLeft: '0%'},0)
+        }
+    },3000)
+
+
+    // let index_three = 1 ;
+    // $('.sliderWrap3').append($('.sliderWrap3 > .s1').clone(true));
+    // setInterval(function(){
+    //      $('.sliderWrap3 > .slider').eq(index_three-1).animate({marginTop: '-300'+'px'})
+    //      index_three++;
+    //      if(index_three==4){
+    //         $('.sliderWrap3 > .slider').animate({marginTop: '0px'})
+    //         index_three=1;
+    //      }
+    // },3000)
 })
